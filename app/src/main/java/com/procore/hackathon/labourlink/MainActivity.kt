be93d59d85da0908a676e8c1f27f99eb5161be14
radhikanambiar <radhika.nambiar@procore.com>
2024-12-09 18:58:18 +0530

    Rename notifications to profile Create profileFragment and edit profile screens

--- app/src/main/java/com/procore/hackathon/labourlink/MainActivity.kt
+++ app/src/main/java/com/procore/hackathon/labourlink/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.procore.hackathon.labourlink
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -8,10 +9,13 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.procore.hackathon.labourlink.databinding.ActivityMainBinding
+import com.procore.hackathon.labourlink.ui.profile.NotificationsViewModel
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val notificationsViewModel : NotificationsViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
